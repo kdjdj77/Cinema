9ae1f7cafaa5d7eb58c880e7f2686862b139e26c
kdjdj77 <51112376+kdjdj77@users.noreply.github.com>
2022-08-31 22:47:09 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,20 @@
+# JSP_project01
+
+★처음 프로젝트 실행 시 해야 할 것
+
+0. db 설정할 것
+    (db name : cinema
+    id	 : korin
+    pw   : 1234)
+
+    DB 설정
+      mysql -u root -p
+      CREATE DATABASE cinema default CHARACTER SET UTF8;
+      CREATE USER 'korin'@'localhost' IDENTIFIED BY '1234';
+      GRANT ALL PRIVILEGES ON cinema.* TO 'korin'@'localhost';
+      FLUSH PRIVILEGES;
+1. 프로젝트 버전 Java-11 로 설정
+2. lib에 jar 파일 넣기
+3. add server(tomcat 경로로 설정)
+4. 프로젝트의 build path library에서 lombok 설정
+5. 프로젝트의 build path library에서 server runtime 설정(add library)
---
+++ src/main/java/controller/UserController.java
@@ -0,0 +1,119 @@
+package controller;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import common.C;
+import service.Service;
+import service.user.LoginService;
+import service.user.RegisterService;
+import service.write.DeleteService;
+import service.write.DetailService;
+import service.write.ListService;
+import service.write.SelectService;
+import service.write.UpdateService;
+import service.write.WriteService;
+
+@WebServlet("/user/*")
+public class UserController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    public UserController() {
+        super();
+    }
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		action(request, response);
+	}
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.setCharacterEncoding("UTF-8"); // �븳湲� �씤肄붾뵫
+		action(request, response);
+	}
+	protected void action(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		System.out.println("\n>> " + getClass().getName() + ".action() 실행");
+		
+		// URL濡쒕��꽣 URI, ContextPath, Command 遺꾨━
+		String uri = request.getRequestURI();   			// --> /JSP18_MVC/board/aaa
+		String conPath = request.getContextPath();  		// --> /JSP18_MVC
+		String command = uri.substring(conPath.length());  	// --> /board/aaa
+		
+		// request method
+		String method = request.getMethod();   // GET, POST, PUT, PATCH, DELETE ...
+
+		// 留� request 留덈떎 異쒕젰 (�뀒�뒪�듃)
+		System.out.println("request: " + uri + " (" + method + ")");  
+		System.out.println("conPath: " + conPath);  
+		System.out.println("command: " + command);
+		
+		
+		// 而⑦듃濡ㅻ윭�뒗 
+		// 1. 而ㅻ㎤�뱶�뿉 �뵲�씪, �빐�떦 Service 濡쒖쭅�쓣 �닔�뻾�쓣 �븯怨�
+		// 2. 寃곌낵瑜� �궡蹂대궪 view瑜� 寃곗젙�븳�떎
+		Service service = null;	// �뼱�뼚�븳 Service 濡쒖쭅�쓣 �닔�뻾�븷吏�
+		String viewPage = null; // �뼱�뼚�븳 �럹�씠吏�(view)瑜� 蹂댁뿬以꾩�
+		
+		switch(command) {
+			case "/user/register":
+				switch(method) {
+					case "GET":
+						C.retrieveRedirectAttribute(request);
+						viewPage = "/user/register.jsp";
+						break;
+					case "POST":
+						service = new RegisterService();
+						service.execute(request,  response);
+		                // redirect 媛� 吏꾪뻾�릺硫� �씠誘� response 媛� commit �맖.
+		                // response 媛� commit �릺吏� �븡�� 寃쎌슦留� jsp forward 吏꾪뻾
+						if (!response.isCommitted())
+							viewPage = "/user/registerOK.jsp";
+						break;
+				}
+				break;
+			case "/user/login":
+				switch(method) {
+					case "GET": // 濡쒓렇�씤 �뤌
+						C.retrieveRedirectAttribute(request);
+						viewPage = "/user/login.jsp";
+						break;
+					case "POST": //濡쒓렇�씤 吏꾪뻾
+						service = new LoginService();
+						service.execute(request, response);
+						
+						// Redirect媛� �씪�뼱�굹吏� �븡怨� 濡쒓렇�씤 �꽦怨듯뻽�떎硫�
+						if (!response.isCommitted()) {
+							// 湲곕낯�쟻�쑝濡� home�쑝濡� redirect�븳�떎
+							String redirectUrl = request.getContextPath() + "/home";
+							
+							// �샊�떆 url prior媛� 議댁옱�뻽�떎硫� �빐�떦 url濡� redirect
+							String urlPrior = C.retrieveUrlPrior(request);
+							if (urlPrior != null) redirectUrl = urlPrior;
+							
+							response.sendRedirect(redirectUrl);
+						}
+						break;
+				}
+				break;
+			case "/user/logout":
+				if (method.equals("POST")) {
+					request.getSession().removeAttribute(C.PRINCIPAL);
+					response.sendRedirect(request.getContextPath() + "/home");
+				}
+				break;
+			case "/user/rejectAuth":
+				viewPage = "/common/rejectAuth.jsp";
+				break;
+		}
+		
+		//�쐞�뿉�꽌 寃곗젙�맂 酉� �럹�씠吏�(viewPage)濡� forward �빐以�
+		if (viewPage != null) {
+			RequestDispatcher dispatcher = 
+				request.getRequestDispatcher("/WEB-INF/views" + viewPage);
+			dispatcher.forward(request, response);
+		}
+	}
+}
