0cba00c2f011b3e748c732bd4932dfe0bffe8f1f
kdjdj77 <51112376+kdjdj77@users.noreply.github.com>
2022-09-14 18:12:39 +0900

    Revert "Merge pull request #33 from kdjdj77/KDJ"
    This reverts commit aec33654833fc4363381bc2a2807e1392e38fafe, reversing
    changes made to a408e7fd1c07ae25e9bf9710a6706e0963320a1a.

--- src/main/java/controller/UserController.java
+++ src/main/java/controller/UserController.java
@@ -25,7 +25,7 @@ public class UserController extends HttpServlet {
 		action(request, response);
 	}
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		request.setCharacterEncoding("UTF-8");
+		request.setCharacterEncoding("UTF-8"); // �븳湲� �씤肄붾뵫
 		action(request, response);
 	}
 	protected void action(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
@@ -39,15 +39,17 @@ public class UserController extends HttpServlet {
 		// request method
 		String method = request.getMethod();   // GET, POST, PUT, PATCH, DELETE ...
 
-
+		// 留� request 留덈떎 異쒕젰 (�뀒�뒪�듃)
 		System.out.println("request: " + uri + " (" + method + ")");  
 		System.out.println("conPath: " + conPath);  
 		System.out.println("command: " + command);
 		
 		
-		
-		Service service = null;	
-		String viewPage = null; 
+		// 而⑦듃濡ㅻ윭�뒗 
+		// 1. 而ㅻ㎤�뱶�뿉 �뵲�씪, �빐�떦 Service 濡쒖쭅�쓣 �닔�뻾�쓣 �븯怨�
+		// 2. 寃곌낵瑜� �궡蹂대궪 view瑜� 寃곗젙�븳�떎
+		Service service = null;	// �뼱�뼚�븳 Service 濡쒖쭅�쓣 �닔�뻾�븷吏�
+		String viewPage = null; // �뼱�뼚�븳 �럹�씠吏�(view)瑜� 蹂댁뿬以꾩�
 		
 		switch(command) {
 			case "/user/register":
@@ -59,6 +61,8 @@ public class UserController extends HttpServlet {
 					case "POST":
 						service = new RegisterService();
 						service.execute(request,  response);
+		                // redirect 媛� 吏꾪뻾�릺硫� �씠誘� response 媛� commit �맖.
+		                // response 媛� commit �릺吏� �븡�� 寃쎌슦留� jsp forward 吏꾪뻾
 						if (!response.isCommitted())
 							viewPage = "/user/registerOK.jsp";
 						break;
@@ -66,17 +70,20 @@ public class UserController extends HttpServlet {
 				break;
 			case "/user/login":
 				switch(method) {
-					case "GET":
+					case "GET": // 濡쒓렇�씤 �뤌
 						C.retrieveRedirectAttribute(request);
 						viewPage = "/user/login.jsp";
 						break;
-					case "POST":
+					case "POST": //濡쒓렇�씤 吏꾪뻾
 						service = new LoginService();
 						service.execute(request, response);
 						
+						// Redirect媛� �씪�뼱�굹吏� �븡怨� 濡쒓렇�씤 �꽦怨듯뻽�떎硫�
 						if (!response.isCommitted()) {
+							// 湲곕낯�쟻�쑝濡� home�쑝濡� redirect�븳�떎
 							String redirectUrl = request.getContextPath() + "/movie/home";
 							
+							// �샊�떆 url prior媛� 議댁옱�뻽�떎硫� �빐�떦 url濡� redirect
 							String urlPrior = C.retrieveUrlPrior(request);
 							if (urlPrior != null) redirectUrl = urlPrior;
 							
